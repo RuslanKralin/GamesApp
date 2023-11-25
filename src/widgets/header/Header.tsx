db0baa036bb088656232c5ba98ad0d35b425eb9a
Ruslan Huligan <kralin.front@gmail.com>
2023-11-25 09:49:54 +0300

    add link for game in result search window

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -11,6 +11,21 @@ import {
 } from '@mui/material'
 
 import SearchIcon from '@mui/icons-material/Search'
+import { ROUTES } from 'shared/consts/routes'
+import { generatePath, useNavigate } from 'react-router-dom'
+
+const linkStyle = {
+    color: 'white',
+    fontWeight: '800',
+    fontSize: '24px',
+    lineHeight: '28px',
+    marginBottom: '16px',
+    cursor: 'pointer',
+    '&:hover': {
+        color: 'grey',
+        transition: 'background-color 0.5s ease', // сделать плавнее
+    },
+}
 
 type Response = {
     count: number
@@ -18,8 +33,11 @@ type Response = {
 }
 
 function Header() {
+    const navigate = useNavigate()
+
     const { REACT_APP_API_ENDPOINT, REACT_APP_API_KEY } = process.env
 
+    const [isOpenWindow, setIsOpenWindow] = useState(false)
     const [searchParams, setSearchParams] = useState('')
     const [searchData, setSearchData] = useState<Response>()
 
@@ -30,6 +48,7 @@ function Header() {
     > = (event: any) => {
         let inputValue = event.target.value
         setSearchParams(inputValue)
+        setIsOpenWindow(true)
         // console.log(inputValue)
         // console.log(searchParams)
         debounseSearch()
@@ -44,10 +63,9 @@ function Header() {
     async function getGamasBySearch(URL: string) {
         const response = await fetch(URL_SEARCH)
         const data = await response.json()
-        // console.log(data)
+        console.log(data)
         setSearchData(data)
     }
-    console.log(searchData?.results.slice(0, 7))
 
     // useEffect(() => {}, [URL_SEARCH])
 
@@ -131,20 +149,99 @@ function Header() {
                         onChange={handleChange}
                         // value={searchParams}
                     />
-                    <Box
-                        sx={{
-                            position: 'absolute',
-                            zIndex: 1000,
-                            top: '70px',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            width: '60rem',
-                            backgroundColor: 'black',
-                            borderRadius: '20px',
-                            padding: '20px 15px',
-                        }}
-                    >
-                        <Box>
+                    {searchData && isOpenWindow === true && (
+                        <Box
+                            sx={{
+                                position: 'absolute',
+                                zIndex: 1000,
+                                top: '70px',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                width: '60rem',
+                                backgroundColor: 'black',
+                                borderRadius: '20px',
+                                padding: '20px 15px',
+                            }}
+                        >
+                            <Box>
+                                <Box
+                                    sx={{
+                                        display: 'flex',
+                                        gap: '10px',
+                                        pb: '15px',
+                                    }}
+                                >
+                                    <Typography sx={{ color: 'white' }}>
+                                        Games
+                                    </Typography>
+                                    <Typography sx={{ color: 'grey' }}>
+                                        {searchData?.count}
+                                    </Typography>
+                                </Box>
+                            </Box>
+                            <Box
+                                sx={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    gap: '15px',
+                                }}
+                            >
+                                {searchData?.results
+                                    .slice(0, 7)
+                                    .map((g: any) => (
+                                        <Box
+                                            sx={{
+                                                display: 'flex',
+                                                gap: '20px',
+                                                alignItems: 'center',
+                                            }}
+                                        >
+                                            <img
+                                                style={{
+                                                    width: '36px',
+                                                    height: '47px',
+                                                }}
+                                                src={g.background_image}
+                                                alt="#"
+                                            />
+                                            <Button
+                                                sx={{
+                                                    all: 'unset', // НЕ ОТМЕНЯЕТ
+                                                    '&:hover': {
+                                                        backgroundColor:
+                                                            'transparent', // Отменяет стиль при наведении
+                                                    },
+                                                    '&:active': {
+                                                        boxShadow: 'none', // Отменяет стиль при нажатии
+                                                    },
+                                                }}
+                                                onClick={() => {
+                                                    navigate(
+                                                        generatePath(
+                                                            ROUTES.ABOUT_GAME,
+                                                            { id: String(g.id) }
+                                                        )
+                                                    )
+                                                    setIsOpenWindow(false)
+                                                }}
+                                            >
+                                                <Link
+                                                    underline="none"
+                                                    sx={linkStyle}
+                                                >
+                                                    {g.name}
+                                                </Link>
+                                            </Button>
+                                        </Box>
+                                    ))}
+                            </Box>
+                            <Box
+                                sx={{
+                                    border: '1px solid grey',
+                                    width: '100%',
+                                    margin: '20px 0',
+                                }}
+                            ></Box>
                             <Box
                                 sx={{
                                     display: 'flex',
@@ -153,62 +250,14 @@ function Header() {
                                 }}
                             >
                                 <Typography sx={{ color: 'white' }}>
-                                    Games
+                                    Collections
                                 </Typography>
                                 <Typography sx={{ color: 'grey' }}>
-                                    {searchData?.count}
+                                    655
                                 </Typography>
                             </Box>
                         </Box>
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                gap: '15px',
-                            }}
-                        >
-                            {searchData?.results.slice(0, 7).map((g: any) => (
-                                <Box
-                                    sx={{
-                                        display: 'flex',
-                                        gap: '20px',
-                                        alignItems: 'center',
-                                    }}
-                                >
-                                    <img
-                                        style={{
-                                            width: '36px',
-                                            height: '47px',
-                                        }}
-                                        src={g.background_image}
-                                        alt="#"
-                                    />
-                                    <Typography sx={{ color: 'white' }}>
-                                        {g.name}
-                                    </Typography>
-                                </Box>
-                            ))}
-                        </Box>
-                        <Box
-                            sx={{
-                                border: '1px solid grey',
-                                width: '100%',
-                                margin: '20px 0',
-                            }}
-                        ></Box>
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                gap: '10px',
-                                pb: '15px',
-                            }}
-                        >
-                            <Typography sx={{ color: 'white' }}>
-                                Collections
-                            </Typography>
-                            <Typography sx={{ color: 'grey' }}>655</Typography>
-                        </Box>
-                    </Box>
+                    )}
                 </Box>
                 <Box
                     sx={{
