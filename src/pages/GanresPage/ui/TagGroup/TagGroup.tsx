4455d7904429dcbcb3591e85331e9affa7e3df80
Ruslan Huligan <kralin.front@gmail.com>
2023-12-13 01:09:06 +0300

    create action page

--- src/pages/GanresPage/ui/TagGroup/TagGroup.tsx
+++ src/pages/GanresPage/ui/TagGroup/TagGroup.tsx
@@ -1,11 +1,6 @@
 import React, { useState } from 'react'
 import { Box, Button, Typography } from '@mui/material'
 
-import GridOnIcon from '@mui/icons-material/GridOn'
-import WebAssetIcon from '@mui/icons-material/WebAsset'
-
-type DisplayOptinsType = 'lines' | 'bigSize'
-
 const btnStyle = {
     borderRadius: '50px',
     color: 'grey',
@@ -19,31 +14,25 @@ const btnStyle = {
     },
 }
 
-const iconStyle = {
-    border: '1px grey',
-    borderRadius: '5px',
-    color: 'white',
-    backgroundColor: '#ffffff12',
-    padding: '5px',
-    width: '50px',
-    height: '50px',
-}
-
 function TagGroup() {
-    const [displayOptions, setDisplayOptions] =
-        useState<DisplayOptinsType>('lines')
-
+    const [gamesData, setGamesData] = useState([])
     return (
         <Box
             sx={{
                 display: 'flex',
-                flexWrap: 'wrap',
-                alignItems: 'center',
-                gap: '10px',
+                flexDirection: 'column',
             }}
         >
-            <Typography sx={{ color: 'grey' }}>Related tags:</Typography>
-            <Box sx={{ display: 'flex', gap: '7px', flexWrap: 'wrap' }}>
+            <Box
+                sx={{
+                    display: 'flex',
+                    gap: '7px',
+                    flexWrap: 'wrap',
+                    alignItems: 'center',
+                    mb: '30px',
+                }}
+            >
+                <Typography sx={{ color: 'grey' }}>Related tags:</Typography>
                 <Button sx={btnStyle}>Singleplayer</Button>
                 <Button sx={btnStyle}>Steam Achievements</Button>
                 <Button sx={btnStyle}>Multiplayer</Button>
@@ -60,82 +49,6 @@ function TagGroup() {
                 <Button sx={btnStyle}>Third Person</Button>
                 <Button sx={btnStyle}>Sci-fi</Button>
                 <Button sx={btnStyle}>FPS</Button>
-            </Box>
-            <Box
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    marginBottom: '30px',
-                }}
-            >
-                <Box sx={{ display: 'flex', gap: '10px' }}>
-                    <Button
-                        sx={{
-                            color: 'primary',
-                            backgroundColor: 'background.btn',
-                            '&:hover': {
-                                backgroundColor: 'background.btnHover',
-                                transition: 'backgroundColor 0.3s ease',
-                            },
-                        }}
-                    >
-                        Order by:{' '}
-                    </Button>
-                    <Button
-                        sx={{
-                            color: 'primary',
-                            backgroundColor: 'background.btn',
-                            '&:hover': {
-                                backgroundColor: 'background.btnHover',
-                                transition: 'backgroundColor 0.3s ease',
-                            },
-                        }}
-                    >
-                        Platforms:{' '}
-                    </Button>
-                </Box>
-                <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                    <Typography sx={{ color: 'grey', marginRight: '20px' }}>
-                        Display options:
-                    </Typography>
-                    <Box sx={{ display: 'flex', gap: '10px' }}>
-                        <Button
-                            sx={{
-                                backgroundColor: 'background.btn',
-                                padding: 0,
-                                minWidth: 0,
-                                '& .MuiButton-label': {
-                                    padding: 0,
-                                },
-                                '&:hover': {
-                                    backgroundColor: 'background.btnHover',
-                                    transition: 'backgroundColor 0.3s ease',
-                                },
-                            }}
-                            onClick={() => setDisplayOptions('lines')}
-                        >
-                            <GridOnIcon style={iconStyle} />
-                        </Button>
-                        <Button
-                            sx={{
-                                backgroundColor: 'background.btn',
-                                padding: 0,
-                                minWidth: 0,
-                                '& .MuiButton-label': {
-                                    padding: 0,
-                                },
-                                '&:hover': {
-                                    backgroundColor: 'background.btnHover',
-                                    transition: 'backgroundColor 0.3s ease',
-                                },
-                            }}
-                            onClick={() => setDisplayOptions('bigSize')}
-                        >
-                            <WebAssetIcon style={iconStyle} />
-                        </Button>
-                    </Box>
-                </Box>
             </Box>
         </Box>
     )
